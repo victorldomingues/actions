c9083556400b852404557353f57e2bbeed7fdef5
Victor Luiz Domingues <victorl.domingues@hotmail.com>
2023-05-09 20:07:11 -0300

    teste action

--- net-packages.py
+++ net-packages.py
@@ -1,6 +1,6 @@
 from os import listdir
 from os.path import isfile, join
-onlyfiles = [f for f in listdir("./") if isfile(join("./", f))]
+onlyfiles = [f for f in listdir("../") if isfile(join("../", f))]
 
 print("hello")
 print(onlyfiles)
