d041affcf05165a56904854707d0eccc04b3b929
Victor Luiz Domingues <victorl.domingues@hotmail.com>
2023-05-09 20:16:18 -0300

    action

--- net-packages.py
+++ net-packages.py
@@ -2,5 +2,11 @@ from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir("../") if isfile(join("../", f))]
 
-print("hello")
+print("file")
 print(onlyfiles)
+
+print("all")
+print(listdir("../"))
+
+print("current")
+print(listdir("."))
